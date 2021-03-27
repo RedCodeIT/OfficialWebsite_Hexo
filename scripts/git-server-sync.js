9891e0893b3ba7b3264d0df704b7de61ca140b9d
wofbi1 <wofbi1@outlook.com>
2021-03-28 00:04:53 +0800

    :sparkles: 同步新消息

--- scripts/git-server-sync.js
+++ scripts/git-server-sync.js
@@ -1,14 +1,22 @@
+// 引入Node.js 自带的 http 模块，并且把它赋值给 http 变量
 const http = require('http');
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+const exec = require('child_process').exec;
+const { ZEvent } = require('zevent');
+const { resolve } = require('path');
 
-const path = require('path')
-const git = require('isomorphic-git')
-
-const fs = require('fs')
-
-
-
+let event = new ZEvent();
+// 创建createServer方法用于接受http客户端请求及返回响应的http服务器程序
 http.createServer(function (req,res){
-    // todo 与git远程仓库同步
+
+    event.send('sync_event'); 
     res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello World\n' + __dirname);
+
 }).listen(8888);
+
+event.on("sync_event", ()=>{
+    exec('cd ../source/_posts');
+    exec('git pull origin main');
+});
